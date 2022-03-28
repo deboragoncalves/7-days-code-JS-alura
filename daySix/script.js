5bb606c509cbaa8130c4c503ce14587112e927f4
Débora Gonçalves <deboracristinapereira123@gmail.com>
2022-03-28 13:40:38 -0300

    Day 6 - Remover produto

--- daySix/script.js
+++ daySix/script.js
@@ -1,16 +1,25 @@
 var containerBuy = document.querySelector("#container-buy");
 var elementBuyList = document.querySelector("#buy-list");
+var containerRemoveProduct = document.querySelector(
+  "#container-product-remove"
+);
 
 var product,
+  productRemove,
   listProducts = new Array();
 
 window.onload = () => {
   containerBuy.style.display = "none";
   elementBuyList.style.display = "none";
+  containerRemoveProduct.style.display = "none";
 };
 
 const changeProduct = () => {
   product = document.querySelector("#product").value;
+};
+
+const changeProductRemove = () => {
+  productRemove = document.querySelector("#productRemove").value;
 };
 
 const sendProduct = () => {
@@ -38,6 +47,44 @@ const sendProduct = () => {
     );
   }
 
+  verificarRemoverProduto();
+};
+
+const verificarRemoverProduto = () => {
+  let checkedWantRemove = document.querySelector(
+    'input[name="wantRemove"]:checked'
+  );
+
+  if (!checkedWantRemove) {
+    exibirListaProdutos();
+    return;
+  } else if (checkedWantRemove.value == "Não") {
+    exibirListaProdutos();
+    return;
+  }
+
+  containerRemoveProduct.style.display = "block";
+
+  if (!productRemove) {
+    exibirListaProdutos();
+    return;
+  }
+
+  if (listProducts.includes(productRemove)) {
+    for (let index = 0; index < listProducts.length; index++) {
+      if (listProducts[index].includes(productRemove)) {
+        listProducts = listProducts.splice(index, 1);
+      }
+    }
+  } else {
+    let productNotFound = document.querySelector("#product-not-found");
+    productNotFound.innerHTML = `O produto a ser removido ${productRemove} não foi encontrado na lista de produtos.`;
+  }
+
+  exibirListaProdutos();
+};
+
+const exibirListaProdutos = () => {
   // Exibir lista
   elementBuyList.innerHTML = "Você deseja comprar os seguintes produtos: <br>";
   elementBuyList.innerHTML += listProducts.join("<br>");
