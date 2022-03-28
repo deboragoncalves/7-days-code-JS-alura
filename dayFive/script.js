6e4712b74b49a2899e772c0e98e36960c38a67b9
Débora Gonçalves <deboracristinapereira123@gmail.com>
2022-03-28 00:25:04 -0300

    Inicio - Day 5

---
+++ dayFive/script.js
@@ -0,0 +1,48 @@
+var containerBuy = document.querySelector("#container-buy");
+var elementBuyList = document.querySelector("#buy-list");
+
+var food;
+var listFruits = new Array();
+var listFries = new Array();
+var listCandies = new Array();
+var listOthers = new Array();
+
+window.onload = () => {
+    containerBuy.style.display = "none";
+    elementBuyList.style.display = "none";
+}
+
+const changeFood = () => {
+    food = document.querySelector("#food").value;
+}
+
+const sendFood = () => {
+    let checkedWantBuy = document.querySelector('input[name="wantBuy"]:checked');
+
+    if (checkedWantBuy) {
+        if (checkedWantBuy.value == "Não") {
+            containerBuy.style.display = "none";
+            return;
+        }
+    } else {
+        containerBuy.style.display = "none";
+        return;
+    }
+
+    containerBuy.style.display = "block";
+
+    let checkedCategory = document.querySelector('input[name="categories"]:checked');
+
+    if (checkedCategory) {
+        if (food) {
+            elementBuyList.innerHTML = "Você deseja comprar os seguintes produtos: <br>";
+            listFruits.push(food);
+            elementBuyList.innerHTML += `Categoria: ${checkedCategory.value}, Produto: ${food}`;
+        } 
+
+        elementBuyList.style.display = "block";
+    }
+
+}
+
+// TODO: vários produtos
