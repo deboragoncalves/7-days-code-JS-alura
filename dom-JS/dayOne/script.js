a396911ac4717e478f3b5f243844917325335cdc
Debora Goncalves <deboracristinapereira123@gmail.com>
2023-05-31 12:03:45 -0300

    Criando layout de acordo com Figma - Day 1 - Alura

--- dom-JS/dayOne/script.js
+++
@@ -1,7 +0,0 @@
-const saveBirthday = e => {
-    e.preventDefault();
-    let name = document.querySelector("#name").value;
-    let birthday = document.querySelector("#birthday").value;
-    console.log("Nome: " + name);
-    console.log("Data de nascimento: " + birthday);
-}
