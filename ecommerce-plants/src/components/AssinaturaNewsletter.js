478c838f6515812ab2cf856b0f3a58c7908426eb
Debora Goncalves <deboracristinapereira123@gmail.com>
2023-06-18 12:18:14 -0300

    Usar styled components - Ecommerce Day 3 - Alura

--- ecommerce-plants/src/components/AssinaturaNewsletter.js
+++ ecommerce-plants/src/components/AssinaturaNewsletter.js
@@ -1,24 +1,106 @@
 import imagePlant from '../assets/image-plant.png';
+import backgroundYellow from '../assets/image-yellow.png';
+import styled from 'styled-components';
 
+const MainContainer = styled.main`
+    display: flex;
+`;
+
+const TextNewsletterContainer = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 585px;
+    height: 462px;
+`;
+
+const TextPlants = styled.span`
+    font-family: 'Montserrat', sans-serif;
+    font-size: 22px;
+    line-height: 27px;
+    color: #202020;
+    opacity: 0.5;
+`;
+
+const TextBestPlants = styled.span`
+    width: 375px;
+    text-align: center;
+    font-family: 'Elsie Swash Caps', sans-serif;
+    font-style: normal;
+    font-weight: 900;
+    font-size: 50px;
+    color: #202020;
+    margin-top: 12px;
+`;
+
+const BiggerTextPlants = styled.span`
+    font-family: 'Montserrat', sans-serif;
+    font-size: 22px;
+    line-height: 27px;
+    color: #202020;
+    opacity: 0.5;
+    width: 381px;
+    height: 99px;
+    margin-top: 24px;
+    font-size: 16px;
+`;
+
+const ContainerEmail = styled.section`
+    display: flex;
+    justify-content: center;
+
+    input {
+        height: 70px;
+        font-family: 'Montserrat', sans-serif;
+        font-size: 16px;
+        line-height: 20px;
+        color: #202020;
+        padding-left: 35px;
+        opacity: 0.3;
+    }
+
+    button {
+        cursor: pointer;
+        width: 194px;
+        height: 75px;
+        border: none;
+        background: #FFCB47;
+        color: #fff;
+        font-family: 'Montserrat';
+        font-size: 16px;
+        line-height: 20px;
+    }
+`;
+
+const ContainerPlantNewsletter = styled.section`
+    background: url(${backgroundYellow});
+    background-repeat: no-repeat;
+
+    img {
+        width: 787px;
+        height: 750px;
+    }
+`;
 
 function AssinaturaNewsletter() {
     return (
-        <main className="container-newsletter">
+        <MainContainer>
             <section>
-                <section className="container-text-newsletter">
-                    <span className="text-plants">Sua casa com as</span>
-                    <span className="text-best-plants">melhores plantas</span>
-                    <span className="text-plants bigger-text-plants">Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.</span>
-                </section>
-                <section className="container-email">
+                <TextNewsletterContainer>
+                    <TextPlants>Sua casa com as</TextPlants>
+                    <TextBestPlants>melhores plantas</TextBestPlants>
+                    <BiggerTextPlants>Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.</BiggerTextPlants>
+                </TextNewsletterContainer>
+                <ContainerEmail>
                     <input type="email" placeholder="Insira seu email"></input>
                     <button type="button">Assinar newsletter</button>
-                </section>
+                </ContainerEmail>
             </section>
-            <section className="container-plant-newsletter">
+            <ContainerPlantNewsletter>
                 <img alt="Planta" src={imagePlant}></img>
-            </section>
-        </main>
+            </ContainerPlantNewsletter>
+        </MainContainer>
     )
 }
 
--- ecommerce-plants/src/components/Menu.js
+++ ecommerce-plants/src/components/Menu.js
@@ -1,17 +1,47 @@
 import logo from '../assets/logo.png';
+import styled from 'styled-components';
+
+const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 48.84px;
+
+    ul li {
+        cursor: pointer;
+    }
+
+    img {
+        padding: 10px 30px;
+        display: flex;
+        align-items: center;
+    }
+`;
+
+const ListMenu = styled.ul`
+    display: flex;
+    height: 20px;
+
+    font-family: 'Montserrat', sans-serif;
+    font-size: 16px;
+    line-height: 20px;
+
+    color: #202020;
+    padding: 10px 30px;
+`;
 
 function Menu() {
     return (
-        <header>
+        <Header>
             <img src={logo} alt="Logo" />
-            <ul className="list-menu">
+            <ListMenu>
                 <li>Como fazer&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;</li>
                 <li>Ofertas&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;</li>
                 <li>Depoimentos&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;</li>
                 <li>Vídeos&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;</li>
                 <li>Meu carrinho</li>
-            </ul>
-        </header>
+            </ListMenu>
+        </Header>
     );
   }
   
