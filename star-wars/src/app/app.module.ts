536c54021c1fc62e5d1329f3f96beeada383f3ef
Debora Goncalves <deboracristinapereira123@gmail.com>
2023-11-03 18:12:57 -0300

    Exibindo dados filmes JSON - Day 2 - Angular

--- star-wars/src/app/app.module.ts
+++ star-wars/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { FilmesComponent } from './filmes/filmes.component';
 import { NaveComponent } from './nave/nave.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { NaveComponent } from './nave/nave.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- star-wars/src/app/filmes/filmes.component.ts
+++ star-wars/src/app/filmes/filmes.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+type Movie = {
+  title: string,
+  director: string,
+  producer: string,
+  date: string
+}
 
 @Component({
   selector: 'app-filmes',
   templateUrl: './filmes.component.html',
   styleUrls: ['./filmes.component.css']
 })
-export class FilmesComponent {
 
+export class FilmesComponent {
+  movies: Movie[] = [];
+  jsonMovies = 'assets/films.json';
+
+  constructor(public http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<any>(this.jsonMovies).subscribe(response => {
+      let allMovies = response.results;
+      for (let movie of allMovies) {
+        this.movies.push({
+          title: movie.title,
+          director: movie.director,
+          producer: movie.producer,
+          date: movie.release_date
+        })
+      };
+    });
+  }
 }
